5733657ee6771ad31bfc34f2abea60fa17257e6b
Brendon Sadler <55864280+sadlerb@users.noreply.github.com>
2020-12-04 01:02:59 -0500

    Update newUser.js

--- scripts/newUser.js
+++ scripts/newUser.js
@@ -4,10 +4,7 @@ window.onload = function(){
     let password = document.getElementById('password');
     let email = document.getElementById('email');
     let submit = document.getElementById('searchbtn');
-    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[a-zA-Z0-9]).{8,}$/;
-    let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    let httpRequest = new XMLHttpRequest();
-
+    let regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[a-zA-Z0-9]).{8,}$/;
 
     submit.addEventListener('click',function(event){
         event.preventDefault();
@@ -15,24 +12,17 @@ window.onload = function(){
         let lastname = lname.value;
         let pass = password.value;
         let mail = email.value;
-        
-        if(passwordRegex.test(pass == false)){
-            alert("Password must contain one letter, one number, one capital letter and consist of 8 characters");
+        var request = new XMLHttpRequest();
+        if(regex.test(pass)){
+            alert("Password accepted")
         }else{
-            if(emailRegex.test(mail == false)){
-                alert("Please enter a vaild email");
-            }else{
-
-                httpRequest.open("GET","http://localhost/Info2180-project2/database/index.php?context=newUser&fname=" + firstname + "&lname=" + lastname + "&password=" + pass + "&email=" + mail);
-                httpRequest.send();
-                httpRequest.onreadystatechange = function(){
-                    if (httpRequest.readyState == 4 && httpRequest.status == 200){
-
-                        alert(httpRequest.responseText);
-                    }
-                }
+            alert("Password must contain one letter, one number, one capital letter and consist of 8 characters");
+        }
+        request.open("POST","http://localhost:8080/database.php?fname=" + firstname + "lname=" + lastname + "email=" + mail + "password=" + pass,true)
+        request.onreadystatechange = function() {
+            if (this.DONE && this.status == 200) {
+                alert("User added.");
             }
-            
         }
     })
 }
